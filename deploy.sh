d703ee575f3b233167338bf1498cce9657b53c5d
Liana Gareeva <liga.94@bk.ru>
2017-03-06 17:18:01 +0300

    deployment

--- deploy.sh
+++ deploy.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
-docker build -t kostyaurysov/sample-node .
-docker push kostyaurysov/sample-node
+docker build -t liana/sample-node .
+docker push liana/sample-node
 
-ssh deploy@35.187.30.81 << EOF
-docker pull kostyaurysov/sample-node:latest
+ssh gareeva_inno@104.196.248.113  << EOF
+docker pull liana/sample-node:latest
 docker stop web || true
 docker rm web || true
-docker rmi kostyaurysov/sample-node:current || true
-docker tag kostyaurysov/sample-node:latest kostyaurysov/sample-node:current
-docker run -d --net app --restart always --name web -p 80:80 kostyaurysov/sample-node:current
+docker rmi liana/sample-node:current || true
+docker tag liana/sample-node:latest liana/sample-node:current
+docker run -d --net app --restart always --name web -p 80:80 liana/sample-node:current
 EOF
